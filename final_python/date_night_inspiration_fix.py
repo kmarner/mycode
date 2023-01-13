c25d0f500b4b5df5916145d9cb14443e60215400
kmarner <keriise.griffin@tlgcohort.com>
2023-01-13 23:17:20 +0000

    Learning how to push data to the screen via debug

---
+++ final_python/date_night_inspiration_fix.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""Dummy-Proof Date Night Idea Generator | Author: Keriise Griffin-Marner
+
+   Description:
+   A script to utilize user input to generate the perfect date night 
+   
+   documentation for the API is available at https://docs.developer.yelp.com/docs/getting-started
+
+   date_night_inspiration.py
+
+   """
+
+
+# to choose random answer from various results based on user input
+import os
+import random
+import requests
+import datetime
+from pprint import pprint
+
+api_key = os.environ.get("YELP_API")
+
+def parameters(location, term, api_key): # diet, price
+    print(location, term)
+
+    headers = {'Authorization': f'Bearer {api_key}'}
+    
+    url='https://api.yelp.com/v3/businesses/search'
+
+    # In the dictionary, term can take values like food, cafes or businesses like McDonalds
+    params = {'term': term,'location':location, 'limit': 5} #, 'radius': radius}
+
+    response=requests.get(url, params=params, headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+   
+        for x in data["businesses"]:
+            print(x["name"])
+            print(x["url"])
+            print()
+
+    else:
+        print(f'Hmmmm... I can\'t seem to find anything that fits those exact responses. Would you like to try again?')
+        print(response.text)
+
+# Main function to run the code
+def main():
+
+    # ??? DO I NEED A TRY/EXCEPT FUNCTION FOR EACH QUESTION TO ENSURE NO OTHER INPUT IS ACCEPTED -- TO BE REPLACED WITH QUESTIONS--OPTIONS WILL BE GIVEN INSTEAD OF MULTIPLE CHOICE--
+    location = input("location:") or "New York City"
+    term = input("term: ") or "seafood"
+    #radius = input("radius: ") or 50
+    #culture = input("culture: ") or "indian"
+    #diet = input("diet: ") or ""
+    #price = input("price: ") or ""
+    # date = input("date: ") or ""
+    parameters(location, term, api_key) # diet, price
+
+main()
